ebca0f1512b2a1bc404b17b64f856422ea6ecefc
miloszKubiak <milypan69@gmail.com>
2022-07-31 13:51:47 +0200

    create footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return <Wrapper>
+		<h5>
+			&copy; {new Date().getFullYear()}
+			<span>ItalianFood</span>
+		</h5>
+		<h5>All rights reserved</h5>
+	</Wrapper>;
 };
 
 export default Footer;
+
+const Wrapper = styled.div`
+	height: 5rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	background: var(--primary-4);
+
+	span {
+		margin: .2rem;
+		color: var(--primary-2);
+	}
+
+	h5 {
+		color: #222;
+		margin: .1rem;
+	}
+`;
