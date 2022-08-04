9b587a084803c5276e96d913e08ba72294bb038c
miloszKubiak <milypan69@gmail.com>
2022-08-04 18:23:43 +0200

    style meal and featuredMeals  component

--- src/components/Meals/FeaturedMeals.jsx
+++ src/components/Meals/FeaturedMeals.jsx
@@ -32,20 +32,39 @@ const FeaturedMeals = () => {
 export default FeaturedMeals;
 
 const Wrapper = styled.section`
-  padding: 5rem 0;
-  background: #f9f9f9;
+	padding: 5rem 0;
+	background: #f9f9f9;
 `;
 
-const Header = styled.div``;
+const Header = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
 
-const Title = styled.h2``;
+const Title = styled.h2`
+	text-align: center;
+	font-size: 3rem;
+	margin-bottom: .5rem;
+	text-transform: capitalize;
+`;
 
-const Underline = styled.div``;
+const Underline = styled.div`
+	background: var(--primary-3);
+	padding: .2rem;
+	width: 15rem;
+`;
 
 const List = styled.div`
-  margin: 4rem auto;
-  width: 90vw;
-  max-width: var(--max-width);
-  display: flex;
-  gap: 3rem;
+	margin: 4rem auto;
+	width: 90vw;
+	max-width: var(--max-width);
+	display: flex;
+	gap: 3rem;
+
+	@media screen and (max-width: 768px) {
+		flex-direction: column;
+		padding: 0 1rem;
+	}
 `;
--- src/components/Meals/Meal.jsx
+++ src/components/Meals/Meal.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { FaInfo } from "react-icons/fa";
 
 const Meal = ({ id, name, image, price }) => {
 	return (
 		<Wrapper>
 			<Container>
-				<Image src={image} alt={name} />
-				<Info to={`/meals/${id}`}>
-					<FaInfo />
-				</Info>
+				<Link to={`/meals/${id}`}>
+					<Image src={image} alt={name} />
+				</Link>
 			</Container>
 			<Footer>
 				<Name>{name}</Name>
@@ -23,12 +21,11 @@ const Meal = ({ id, name, image, price }) => {
 export default Meal;
 
 const Wrapper = styled.article`
-  flex: 1;
+	flex: 1;
 `;
 
 const Container = styled.div`
 	position: relative;
-	background: var(--primary-1);
 	border-radius: 0.5rem;
 `;
 
@@ -40,35 +37,9 @@ const Image = styled.img`
 	border-radius: 0.5rem;
 	transition: var(--transition);
 
-  &:hover {
-    opacity: .5;
-  }
-`;
-
-const Info = styled(Link)`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background: var(--primary-3);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 3rem;
-  width: 3rem;
-  border-radius: 50%;
-  transition: var(--transition);
-  opacity: 0;
-  cursor: pointer;
-
-  svg {
-    font-size: 1.5rem;
-    color: var(--primary-2);
-  }
-
-  &:hover {
-    opacity: 1;
-  }
+	&:hover {
+		opacity: 0.5;
+	}
 `;
 
 const Footer = styled.footer`
