7ab93b04492669d7f844b53b79ab924654d38034
miloszKubiak <milypan69@gmail.com>
2022-08-21 11:00:53 +0200

    add axios interceptors

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -36,12 +36,36 @@ const AuthContext = React.createContext();
 export const AuthProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
 
+	///axios custom instance
 	const authFetch = axios.create({
 		baseURL: "/api/v1",
-		headers: {
-			Authorization: `Bearer ${state.token}`,
-		},
 	});
+
+	//request interceptor
+	authFetch.interceptors.request.use(
+		(config) => {
+			config.headers.common["Authorization"] = `Bearer ${state.token}`;
+			return config;
+		},
+		(error) => {
+			return Promise.reject(error);
+		}
+	);
+
+	//response interceptor
+	authFetch.interceptors.response.use(
+		(response) => {
+			return response
+		},
+		(error) => {
+			console.log(error.response);
+			if (error.response.status === 401) {
+				console.log("AUTH ERROR");
+			}
+			return Promise.reject(error)
+		}
+	)
+
 
 	const displayAlert = () => {
 		dispatch({ type: DISPLAY_ALERT });
