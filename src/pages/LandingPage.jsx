854009c7c81612283e9671517e94989d2ad3f96c
miloszKubiak <milypan69@gmail.com>
2022-09-09 12:47:54 +0200

    styling landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -41,6 +41,12 @@ const Logo = styled.div`
 	margin: 0 auto;
 	max-width: var(--max-width);
 	width: 90vw;
+
+	h1 {
+		letter-spacing: var(--spacing);
+		text-transform: capitalize;
+		color: var(--primary-3);
+	}
 `;
 
 const Container = styled.div`
@@ -50,6 +56,7 @@ const Container = styled.div`
 	margin: 0 auto;
 	display: flex;
 	align-items: center;
+	gap: 1rem;
 
 	img {
 		width: 100%;
@@ -59,24 +66,65 @@ const Container = styled.div`
 `;
 
 const Info = styled.div`
-  flex: 1;
+	flex: 1;
+
+	a {
+		margin-top: 1rem;
+		display: inline-block;
+		padding: 0.8rem 1rem;
+		background: var(--primary-3);
+		color: var(--primary-2);
+		text-decoration: none;
+		border-radius: 0.4rem;
+		text-transform: capitalize;
+		letter-spacing: var(--spacing);
+		transition: var(--transition);
+		font-weight: bold;
+		font-size: 1.2rem;
+
+		&:hover {
+			color: var(--primary-3);
+			background: var(--primary-1);
+		}
+	}
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+	text-transform: capitalize;
+	font-size: 3rem;
 
-const Description = styled.p``;
+	span {
+		color: var(--primary-3);
+	}
+`;
+
+const Description = styled.p`
+	margin: 1rem 0;
+	letter-spacing: var(--spacing);
+`;
 
 const ImgContainer = styled.div`
   flex: 2;
 `;
 
 const Wrapper = styled.main`
-  background: var(--primary-1);
+	background: var(--primary-2);
 
-	@media screen and (min-width: 992px) {
+	@media screen and (max-width: 768px) {
 		${Container} {
-			grid-template-columns: 1fr 1fr;
-			column-gap: 2rem;
+			flex-direction: column;
+		}
+		${Info} {
+			margin-top: 6rem;
+		}
+		${Title} {
+			font-size: 2.5rem;
+		}
+		${Description} {
+			font-size: 0.8rem;
+		}
+		${ImgContainer} {
+			flex: 1;
 		}
 	}
 `;
