e9482a4a6eeafd2918ac5d48a03a8b32f2909f0f
miloszKubiak <milypan69@gmail.com>
2022-09-27 16:09:39 +0200

    add card info component

--- src/components/AdminPanel/index.js
+++ src/components/AdminPanel/index.js
@@ -2,10 +2,12 @@ import AdminNavbar from "./AdminNavbar";
 import AdminBigSidebar from "./AdminBigSidebar";
 import AdminSmallSidebar from "./AdminSmallSidebar";
 import AdminAlert from "./AdminAlert";
+import CardInfo from "./CardInfo";
 
 export {
 	AdminNavbar,
 	AdminBigSidebar,
 	AdminSmallSidebar,
 	AdminAlert,
+	CardInfo,
 };
