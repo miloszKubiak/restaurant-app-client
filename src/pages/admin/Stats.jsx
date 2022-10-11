1bc927d394d46cc5497f05dcfdca815def53213e
miloszKubiak <milypan69@gmail.com>
2022-10-11 09:08:10 +0200

    add fetch stats function

--- src/pages/admin/Stats.jsx
+++ src/pages/admin/Stats.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import authFetch from "../../utils/axios";
 
 const Stats = () => {
-	return <div>Stats</div>;
+	const [isLoading, setIsLoading] = useState(false);
+	const [stats, setStats] = useState({});
+
+	const showStats = async () => {
+		try {
+			setIsLoading(true);
+			const { data } = await authFetch.get("/orders/all-stats");
+			setStats(data.defaultStats);
+			console.log(data);
+			setIsLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		showStats();
+	}, []);
+
+	return <Wrapper>stats</Wrapper>;
 };
 
 export default Stats;
+
+const Wrapper = styled.div``;
