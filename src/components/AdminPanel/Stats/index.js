b17cad6fb8168824758e9ffe0fb1be87ea0306c1
miloszKubiak <milypan69@gmail.com>
2022-10-12 09:56:51 +0200

    create components to the stats page

---
+++ src/components/AdminPanel/Stats/index.js
@@ -0,0 +1,5 @@
+import StatsContainer from "./StatsContainer";
+import StatsItem from "./StatsItem";
+import ChartsContainer from "./ChartsContainer";
+
+export { StatsContainer, StatsItem, ChartsContainer };
--- src/pages/admin/Stats.jsx
+++ src/pages/admin/Stats.jsx
@@ -6,27 +6,69 @@ import authFetch from "../../utils/axios";
 
 const Stats = () => {
 	const [isLoading, setIsLoading] = useState(false);
-	const [stats, setStats] = useState({});
+	const [stats, setStats] = useState([]);
+	const defaultStats = [
+		{
+			title: "orders declined",
+			count: stats.declined || 0,
+			color: "red",
+			background: "#fff",
+		},
+		{
+			title: "orders sent",
+			count: stats.sent || 0,
+			color: "yellow",
+			background: "#fff",
+		},
+		{
+			title: "orders paid",
+			count: stats.paid || 0,
+			color: "green",
+			background: "#fff",
+		},
+		{
+			title: "orders delivered",
+			count: stats.delivered || 0,
+			color: "blue",
+			background: "#fff",
+		},
+	];
 
 	const showStats = async () => {
 		try {
 			setIsLoading(true);
 			const { data } = await authFetch.get("/orders/all-stats");
 			setStats(data.defaultStats);
-			console.log(data);
 			setIsLoading(false);
 		} catch (error) {
 			console.log(error);
 		}
 	};
-
+	console.log(stats);
 	useEffect(() => {
 		showStats();
 	}, []);
 
-	return <Wrapper>stats</Wrapper>;
+	return (
+		<Wrapper>
+			stats
+			{defaultStats.map((item, index) => {
+				return (
+					<li key={index}>
+						<p>{item.title}</p>
+						<p>{item.count}</p>
+					</li>
+				)
+			})}
+		</Wrapper>
+	);
 };
 
 export default Stats;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	li {
+		margin: 1rem;
+		background: red
+	}
+`;
